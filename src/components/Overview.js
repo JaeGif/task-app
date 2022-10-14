0e383f383a34157fbb0bfa7d0adf27c8a4cd5d20
Jae Gifford <jae.giff@outlook.com>
2022-10-14 14:01:47 +0800

    overview added

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,1 +1,16 @@
 // renders the tasks
+import React from 'react';
+
+const Overview = (props) => {
+  const { tasks } = props;
+
+  return (
+    <ul>
+      {tasks.map((task) => {
+        return <li>{task.text}</li>;
+      })}
+    </ul>
+  );
+};
+
+export default Overview;
