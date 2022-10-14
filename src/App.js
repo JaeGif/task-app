5270ef3c75e5b17e32906a3cdc242b0823642399
Jae Gifford <jae.giff@outlook.com>
2022-10-14 13:48:38 +0800

    task-app skeleton add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-function App() {
-  return <div></div>;
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      task: { text: '' },
+      tasks: [],
+    };
+  }
+  render() {
+    const { task, tasks } = this.state;
+    return (
+      <div>
+        <form>
+          <label htmlFor='taskInput'>Enter Task</label>
+          <input type='text' id='task-input'></input>
+          <button type='submit'>Add Task</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
