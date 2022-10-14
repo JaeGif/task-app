2a637ee02f2919e9669172ee4bb16abc43ee500f
Jae Gifford <jae.giff@outlook.com>
2022-10-14 14:01:10 +0800

    App.js state changes added

--- src/App.js
+++ src/App.js
@@ -8,13 +8,33 @@ class App extends Component {
       tasks: [],
     };
   }
+  handleChange = (e) => {
+    // uses setState to change the text to the value of the input
+    this.setState({
+      task: { text: e.target.value },
+    });
+  };
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    // we don't use the push method because that wouldl cause an error with altering the state directly.
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: { text: '' },
+    });
+  };
   render() {
     const { task, tasks } = this.state;
     return (
       <div>
-        <form>
+        <form onSubmit={this.onSubmitTask}>
           <label htmlFor='taskInput'>Enter Task</label>
-          <input type='text' id='task-input'></input>
+          <input
+            type='text'
+            id='task-input'
+            onChange={this.handleChange}
+            // we have to specify a value attribute
+            value={task.text}
+          ></input>
           <button type='submit'>Add Task</button>
         </form>
       </div>
