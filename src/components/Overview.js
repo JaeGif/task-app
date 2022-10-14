b7e130703ff0e12b4e4d3095656e3c883cd48739
Jae Gifford <jae.giff@outlook.com>
2022-10-14 14:18:56 +0800

    uniqid import

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,11 +1,13 @@
 // renders the tasks
 import React from 'react';
+import uniqid from 'uniqid';
 
 const Overview = (props) => {
   const { tasks } = props;
 
   return (
     <ul>
+      {/* Tasks are mapped to list items */}
       {tasks.map((task) => {
         return <li>{task.text}</li>;
       })}
